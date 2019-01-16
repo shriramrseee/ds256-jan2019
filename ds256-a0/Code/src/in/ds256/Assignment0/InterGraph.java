3d62417773c27c0f3416401124968bb99bd9140e
Shriram Ramesh <shriramrseee@gmail.com>
2019-01-17 02:54:03 +0530

    Baby steps

--- ds256-a0/Code/src/in/ds256/Assignment0/InterGraph.java
+++ ds256-a0/Code/src/in/ds256/Assignment0/InterGraph.java
@@ -9,23 +9,23 @@ import org.apache.spark.api.java.JavaSparkContext;
  * DS-256 Assignment 0
  * Code for generating interaction graph
  */
-public class InterGraph  {
-	
-	public static void main(String[] args) throws IOException {	
-		
-		String inputFile = args[0]; // Should be some file on HDFS
-		String vertexFile = args[1]; // Should be some file on HDFS
-		String edgeFile = args[2]; // Should be some file on HDFS
-		
-		SparkConf sparkConf = new SparkConf().setAppName("InterGraph");
-		JavaSparkContext sc = new JavaSparkContext(sparkConf);
+public class InterGraph {
 
-		/**
-		 * Code goes here
-	 	 */
-		
-		sc.stop();
-		sc.close();
-	}
+    public static void main(String[] args) throws IOException {
+
+        String inputFile = args[0]; // Should be some file on HDFS
+        String vertexFile = args[1]; // Should be some file on HDFS
+        String edgeFile = args[2]; // Should be some file on HDFS
+
+        SparkConf sparkConf = new SparkConf().setAppName("InterGraph");
+        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+
+        /**
+         * Code goes here
+         */
+
+        sc.stop();
+        sc.close();
+    }
 
 }
--- ds256-a0/Code/src/in/ds256/Assignment0/TopCoOccurrence.java
+++ ds256-a0/Code/src/in/ds256/Assignment0/TopCoOccurrence.java
@@ -9,20 +9,20 @@ import org.apache.spark.api.java.JavaSparkContext;
  * DS-256 Assignment 0
  * Code for finding frequent co-occuring hash-tags
  */
-public class ​TopCoOccurrence  {
-	
-	public static void main(String[] args) throws IOException {	
-		
+public class TopCoOccurrence  {
+
+	public static void main(String[] args) throws IOException {
+
 		String inputFile = args[0]; // Should be some file on HDFS
 		String outputFile = args[1]; // Should be some file on HDFS
-		
+
 		SparkConf sparkConf = new SparkConf().setAppName("​TopCoOccurrence");
 		JavaSparkContext sc = new JavaSparkContext(sparkConf);
 
 		/**
 		 * Code goes here
-	 	 */			
-		
+	 	 */
+
 		sc.stop();
 		sc.close();
 	}

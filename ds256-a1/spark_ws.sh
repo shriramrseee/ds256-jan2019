71e34b9c32121897fad12013faf4bdb51312e176
Shriram Ramesh <shriramrseee@gmail.com>
2019-03-06 15:50:45 +0530

    Refactored Spark Code

--- ds256-a1/spark_ws.sh
+++ ds256-a1/spark_ws.sh
@@ -1,14 +1,21 @@
+#!/bin/bash
 
 # WCC
-spark-submit --class in.ds256.Assignment1.Spark.wcc --master yarn --deploy-mode cluster --num-executors 1 --driver-memory 512m --executor-memory 8G --executor-cores 4  Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/cit-Patents.txt hdfs:///user/shriramr/output0.txt 8&
-spark-submit --class in.ds256.Assignment1.Spark.wcc --master yarn --deploy-mode cluster --num-executors 2 --driver-memory 512m --executor-memory 8G --executor-cores 4  Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/soc-livj.txt hdfs:///user/shriramr/output1.txt 16&
-spark-submit --class in.ds256.Assignment1.Spark.wcc --master yarn --deploy-mode cluster --num-executors 3 --driver-memory 512m --executor-memory 8G --executor-cores 4  Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/USA-road-d.USA.gr hdfs:///user/shriramr/output2.txt 32&
+spark-submit --class in.ds256.Assignment1.Spark.wcc --master yarn --deploy-mode cluster --num-executors 1 --driver-memory 512m --executor-memory 8G --executor-cores 4  Code/target/Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/cit-Patents.txt hdfs:///user/shriramr/output0.txt 8 > log0.txt &
+spark-submit --class in.ds256.Assignment1.Spark.wcc --master yarn --deploy-mode cluster --num-executors 4 --driver-memory 512m --executor-memory 8G --executor-cores 4  Code/target/Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/soc-livj.txt hdfs:///user/shriramr/output1.txt 32 > log1.txt &
+spark-submit --class in.ds256.Assignment1.Spark.wcc --master yarn --deploy-mode cluster --num-executors 7 --driver-memory 512m --executor-memory 8G --executor-cores 4  Code/target/Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/orkut.txt hdfs:///user/shriramr/output2.txt 56 > log2.txt &
 
 # Conductance
-
-
+spark-submit --class in.ds256.Assignment1.Spark.conductance --master yarn --deploy-mode cluster --num-executors 1 --driver-memory 512m --executor-memory 8G --executor-cores 4  Code/target/Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/cit-Patents.txt hdfs:///user/shriramr/output0.txt 8 > log0.txt &
+spark-submit --class in.ds256.Assignment1.Spark.conductance --master yarn --deploy-mode cluster --num-executors 4 --driver-memory 512m --executor-memory 8G --executor-cores 4  Code/target/Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/soc-livj.txt hdfs:///user/shriramr/output1.txt 32 > log1.txt &
+spark-submit --class in.ds256.Assignment1.Spark.conductance --master yarn --deploy-mode cluster --num-executors 7 --driver-memory 512m --executor-memory 8G --executor-cores 4  Code/target/Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/orkut.txt hdfs:///user/shriramr/output2.txt 56 > log2.txt &
 
 # PR
-
+spark-submit --class in.ds256.Assignment1.Spark.pr --master yarn --deploy-mode cluster --num-executors 1 --driver-memory 512m --executor-memory 8G --executor-cores 4  Code/target/Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/cit-Patents.txt hdfs:///user/shriramr/output0.txt 8 > log0.txt &
+spark-submit --class in.ds256.Assignment1.Spark.pr --master yarn --deploy-mode cluster --num-executors 4 --driver-memory 512m --executor-memory 8G --executor-cores 4  Code/target/Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/soc-livj.txt hdfs:///user/shriramr/output1.txt 32 > log1.txt &
+spark-submit --class in.ds256.Assignment1.Spark.pr --master yarn --deploy-mode cluster --num-executors 7 --driver-memory 512m --executor-memory 8G --executor-cores 4  Code/target/Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/orkut.txt hdfs:///user/shriramr/output2.txt 56 > log2.txt &
 
 # Span
+spark-submit --class in.ds256.Assignment1.Spark.span --master yarn --deploy-mode cluster --num-executors 1 --driver-memory 512m --executor-memory 8G --executor-cores 4  Code/target/Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/cit-Patents.txt hdfs:///user/shriramr/output0.txt 8 > log0.txt &
+spark-submit --class in.ds256.Assignment1.Spark.span --master yarn --deploy-mode cluster --num-executors 4 --driver-memory 512m --executor-memory 8G --executor-cores 4  Code/target/Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/soc-livj.txt hdfs:///user/shriramr/output1.txt 32 > log1.txt &
+spark-submit --class in.ds256.Assignment1.Spark.span --master yarn --deploy-mode cluster --num-executors 7 --driver-memory 512m --executor-memory 8G --executor-cores 4  Code/target/Assignment1-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///user/ds256/a1/orkut.txt hdfs:///user/shriramr/output2.txt 56 > log2.txt &

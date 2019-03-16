3124f5f224d32381bfce884e2241f22eb41468da
Shriram Ramesh <shriramrseee@gmail.com>
2019-03-16 13:33:11 +0530

    Add scripts for edge containers

---
+++ project/edge/kill_process.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "----------Killing Process-----------"
+
+for name in `docker ps --format "{{.Names}}" `;
+  do
+         echo "Killing Process in "${name}
+         docker exec -w / -d $name bash -c "kill -- -1" &
+done;

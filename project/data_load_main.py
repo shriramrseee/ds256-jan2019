3a4d4cd13506f2d70b47b48ff8e86f2cfc1eff50
Shriram Ramesh <shriramrseee@gmail.com>
2019-04-25 22:03:20 +0530

    Project Code update

--- project/data_load_main.py
+++ project/data_load_main.py
@@ -1,12 +1,12 @@
-from data_load.loaders import load_fact, load_date, load_literal
+from loaders import load_fact, load_date, load_literal
 
 remote = "ws://35.200.133.241:8182/gremlin"
 local = "ws://localhost:8182/gremlin"
 
 # Load into Cloud server
-load_fact(remote, "../local_data/yagoFacts.tsv")
-load_date(remote, "../local_data/yagoDateFacts.tsv")
-load_literal(remote, "../local_data/yagoLiteralFacts.tsv")
+load_fact(remote, "local_data/yagoFacts.tsv")
+load_date(remote, "local_data/yagoDateFacts.tsv")
+load_literal(remote, "local_data/yagoLiteralFacts.tsv")
 
 # Test
 # load_fact(local, "../local_data/yagoFacts.tsv")
